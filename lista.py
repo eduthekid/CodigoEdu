5e7100ed5aafd15e9cd45f822f56d02fe0488799
Eduardo O Mala <eduardohenriquedebarrosoliveir@gmail.com>
2025-01-22 14:47:36 -0400

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,21 @@
+#lista = [1,2,3,'python', 'chuva',5,6]
+
+#print(lista[:-1])
+
+#a = ('Segunda', 'Terça', 'Quarta', 'Quinta', lista)
+#print(a)
+
+#range(inicio, fim)
+
+#sequencia = range(1, 3)
+#print(sequencia)
+
+#for valor in range(1,3):
+#    print(valor)
+
+#frutas = {'laranja', 'banana', 'uva', 'pera', 'laranja', 'uva', 'abacate'}
+#print(frutas)
+
+pessoa = {'nome':'Ana', 'idade':20, 'cidade':'São Paulo'}
+
+    
---
+++ strings.py
@@ -0,0 +1,17 @@
+#Exercicio Numero 01
+# 1 - Considere a string A = "Um elefante incomoda muita gente"
+# Que fatia corresponde a "elefante incomoda muita gente"
+#a = "Um elefante incomoda muita gente"
+#print(a[3:20])
+
+
+
+#Exercicio Numero 02
+# 2 - Escreva um programa que solicite uma frase ao usuário e escreva a
+#frase toda em maiúscula e sem espaços em branco.
+frase = input("Escreva uma frase: ")
+frase_sem_espaco = frase.replace(' ','')
+frase_maiuscula = frase_sem_espaco.upper()
+print(frase_maiuscula)
+
+
